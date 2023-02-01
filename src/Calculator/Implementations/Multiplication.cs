3366b73769b9b90cfedd1a09320c72cbe05f8bc6
Raman Maksimchuk <dotnet044@gmail.com>
2023-02-01 21:21:38 +0300

    Multiplication

---
+++ src/Calculator/Implementations/Multiplication.cs
@@ -0,0 +1,90 @@
+﻿using RamanM.Properti.Calculator.Interfaces;
+
+namespace RamanM.Properti.Calculator.Implementations;
+
+public class Multiplication : IBinaryOperation
+{
+    private double? value;
+
+    private Multiplication()
+    {
+        value = null;
+        Left = null;
+        Right = null;
+        Parent = null;
+    }
+
+    public Multiplication(double left, double right)
+        : this()
+    {
+        Left = new Constant(left);
+        Right = new Constant(right);
+        Left.Parent = Right.Parent = this;
+    }
+
+    public Multiplication(IOperation left, double right)
+        : this()
+    {
+        Left = left;
+        Right = new Constant(right);
+        Left.Parent = Right.Parent = this;
+    }
+
+    public Multiplication(double left, IOperation right)
+        : this()
+    {
+        Left = new Constant(left);
+        Right = right;
+        Left.Parent = Right.Parent = this;
+    }
+
+    public Multiplication(IOperation left, IOperation right)
+        : this()
+    {
+        Left = left;
+        Right = right;
+        left.Parent = right.Parent = this;
+    }
+
+    public IOperation Left { get; private set; }
+
+    public IOperation Right { get; private set; }
+
+    public IOperation Parent { get; set; }
+
+    public string Print()
+    {
+        var left = Left.Print();
+        var right = Right.Print();
+        double val = value.HasValue ? value.Value : ToResult();
+        var result = Parent == null ? $" = {val}" : string.Empty;
+
+        return $"({left} * {right}){result}";
+    }
+
+    public string PrintSentence()
+    {
+        var left = Left.PrintSentence();
+        var right = Right.PrintSentence();
+        double val = value.HasValue ? value.Value : ToResult();
+        var result = Parent == null ? $" is {val}" : string.Empty;
+
+        return $"{nameof(Multiplication).ToLower()} of {left} and {right}{result}";
+    }
+
+    public double ToResult()
+    {
+        if (!value.HasValue)
+        {
+            var l = Left.ToResult();
+            var r = Right.ToResult();
+            value = Apply(l, r);
+        }
+
+        return (double)value;
+    }
+    public static double Apply(double left, double right)
+    {
+        return left * right;
+    }
+}
---
+++ tst/Calculator/MultiplicationTests.cs
@@ -0,0 +1,87 @@
+﻿using RamanM.Properti.Calculator.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RamanM.Properti.Calculator.Tests;
+
+public class MultiplicationTests
+{
+    [Fact]
+    public void ToResult_Example_ReturnsTheProduct()
+    {
+        // Arrange
+        var sut = new Multiplication(5.0, 1.1);
+        double expected = 5.5;
+
+        // Act
+        var actual = sut.ToResult();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Print_NoParent_PrintsFinalEquality()
+    {
+        // Arrange
+        double left = 5.0D, right = 1.1D, product = 5.5D;
+        var sut = new Multiplication(left, right);
+        string expected = $"({left} * {right}) = {product}";
+
+        // Act
+        var actual = sut.Print();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Print_WithParent_PrintsWithoutResult()
+    {
+        // Arrange
+        double left = 5.0D, right = 1.1D;
+        var sut = new Multiplication(left, right);
+        sut.Parent = new Constant(0);
+        string expected = $"({left} * {right})";
+
+        // Act
+        var actual = sut.Print();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void PrintSentence_NoParent_PrintsFinalSentence()
+    {
+        // Arrange
+        double left = 5.0D, right = 1.1D, product = 5.5D;
+        var sut = new Multiplication(left, right);
+        string expected = $"{nameof(Multiplication).ToLower()} of {left} and {right} is {product}";
+
+        // Act
+        var actual = sut.PrintSentence();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void PrintSentence_WithParent_PrintsWell()
+    {
+        // Arrange
+        double left = 5.0D, right = 1.1D;
+        var sut = new Multiplication(left, right);
+        sut.Parent = new Constant(0);
+        string expected = $"{nameof(Multiplication).ToLower()} of {left} and {right}";
+
+        // Act
+        var actual = sut.PrintSentence();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
