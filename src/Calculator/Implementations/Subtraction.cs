b31fc6ff614ae4806dc4a6bb9d294fc1bb128f82
Raman Maksimchuk <dotnet044@gmail.com>
2023-02-01 21:00:26 +0300

    Subtraction

---
+++ src/Calculator/Implementations/Subtraction.cs
@@ -0,0 +1,94 @@
+﻿using RamanM.Properti.Calculator.Interfaces;
+
+namespace RamanM.Properti.Calculator.Implementations;
+
+/// <summary>
+/// Subtraction of two operations.
+/// </summary>
+public class Subtraction : IBinaryOperation
+{
+    private double? value;
+
+    private Subtraction()
+    {
+        value = null;
+        Left = null;
+        Right = null;
+        Parent = null;
+    }
+
+    public Subtraction(double left, double right)
+        : this()
+    {
+        Left = new Constant(left);
+        Right = new Constant(right);
+        Left.Parent = Right.Parent = this;
+    }
+
+    public Subtraction(IOperation left, double right)
+        : this()
+    {
+        Left = left;
+        Right = new Constant(right);
+        Left.Parent = Right.Parent = this;
+    }
+
+    public Subtraction(double left, IOperation right)
+        : this()
+    {
+        Left = new Constant(left);
+        Right = right;
+        Left.Parent = Right.Parent = this;
+    }
+
+    public Subtraction(IOperation left, IOperation right)
+       : this()
+    {
+        Left = left;
+        Right = right;
+        left.Parent = right.Parent = this;
+    }
+
+    public IOperation Left { get; private set; }
+
+    public IOperation Right { get; private set; }
+
+    public IOperation Parent { get; set; }
+
+    public string Print()
+    {
+        var left = Left.Print();
+        var right = Right.Print();
+        double val = value.HasValue ? value.Value : ToResult();
+        var result = Parent == null ? $" = {val}" : string.Empty;
+
+        return $"({left} - {right}){result}";
+    }
+
+    public string PrintSentence()
+    {
+        var left = Left.PrintSentence();
+        var right = Right.PrintSentence();
+        double val = value.HasValue ? value.Value : ToResult();
+        var result = Parent == null ? $" is {val}" : string.Empty;
+
+        return $"{nameof(Subtraction).ToLower()} of {left} and {right}{result}";
+    }
+
+    public double ToResult()
+    {
+        if (!value.HasValue)
+        {
+            var l = Left.ToResult();
+            var r = Right.ToResult();
+            value = Apply(l, r);
+        }
+
+        return (double)value;
+    }
+
+    public static double Apply(double left, double right)
+    {
+        return left - right;
+    }
+}
---
+++ tst/Calculator/SubtractionTests.cs
@@ -0,0 +1,82 @@
+﻿using RamanM.Properti.Calculator.Implementations;
+
+namespace RamanM.Properti.Calculator.Tests;
+
+public class SubtractionTests
+{
+    [Fact]
+    public void ToResult_Example_ReturnsTheSum()
+    {
+        // Arrange
+        var sut = new Subtraction(5.5, 1.3);
+        double expected = 4.2;
+
+        // Act
+        var actual = sut.ToResult();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Print_NoParent_PrintsFinalEquality()
+    {
+        // Arrange
+        double left = 5.5D, right = 1.3D, difference = 4.2D;
+        var sut = new Subtraction(left, right);
+        string expected = $"({left} - {right}) = {difference}";
+
+        // Act
+        var actual = sut.Print();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Print_WithParent_PrintsWithoutResult()
+    {
+        // Arrange
+        double left = 5.5D, right = 1.3D;
+        var sut = new Subtraction(left, right);
+        sut.Parent = new Constant(0);
+        string expected = $"({left} - {right})";
+
+        // Act
+        var actual = sut.Print();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void PrintSentence_NoParent_PrintsFinalSentence()
+    {
+        // Arrange
+        double left = 5.5D, right = 1.3D, difference = 4.2D;
+        var sut = new Subtraction(left, right);
+        string expected = $"{nameof(Subtraction).ToLower()} of {left} and {right} is {difference}";
+
+        // Act
+        var actual = sut.PrintSentence();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void PrintSentence_WithParent_PrintsWell()
+    {
+        // Arrange
+        double left = 5.5D, right = 1.3D;
+        var sut = new Subtraction(left, right);
+        sut.Parent = new Constant(0);
+        string expected = $"{nameof(Subtraction).ToLower()} of {left} and {right}";
+
+        // Act
+        var actual = sut.PrintSentence();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
