7b7d140a070ab2452447c7bdcd8ebeca490a5a62
Raman Maksimchuk <dotnet044@gmail.com>
2023-02-01 23:05:25 +0300

    Faculty

---
+++ src/Calculator/Implementations/Faculty.cs
@@ -0,0 +1,70 @@
+﻿using RamanM.Properti.Calculator.Interfaces;
+
+namespace RamanM.Properti.Calculator.Implementations;
+
+public class Faculty : IUnaryOperation
+{
+    private long? value;
+
+    private Faculty()
+    {
+        value = null;
+        Operand = null;
+        Parent = null;
+    }
+
+    public Faculty(int operand)
+        : this()
+    {
+        Operand = new Constant(operand);
+        Operand.Parent = this;
+    }
+
+    public Faculty(IOperation operation)
+        : this()
+    {
+        Operand = operation;
+        Operand.Parent = this;
+    }
+
+    public IOperation Operand { get; private set; }
+
+    public IOperation Parent { get; set; }
+
+    public string Print()
+    {
+        var operand = Operand.Print();
+        double val = value.HasValue ? value.Value : ToResult();
+        var result = Parent == null ? $" = {val}" : string.Empty;
+
+        return $"({operand}!){result}";
+    }
+
+    public string PrintSentence()
+    {
+        var operand = Operand.PrintSentence();
+        double val = value.HasValue ? value.Value : ToResult();
+        var result = Parent == null ? $" is {val}" : string.Empty;
+
+        return $"{nameof(Faculty).ToLower()} of {operand}{result}";
+    }
+
+    public double ToResult()
+    {
+        if (!value.HasValue)
+        {
+            int opr = (int)Operand.ToResult();
+            value = Apply(opr);
+        }
+
+        return (double)value;
+    }
+
+    public static long Apply(int operand)
+    {
+        if (operand <= 1)
+            return 1; // neutral element
+
+        return Apply(operand - 1) * operand;
+    }
+}
---
+++ tst/Calculator/FacultyTests.cs
@@ -0,0 +1,86 @@
+﻿using RamanM.Properti.Calculator.Implementations;
+
+namespace RamanM.Properti.Calculator.Tests;
+
+public class FacultyTests
+{
+    [Fact]
+    public void ToResult_Example_ReturnsTheFactorial()
+    {
+        // Arrange
+        var sut = new Faculty(4);
+        long expected = 1 * 2 * 3 * 4;
+
+        // Act
+        long actual = (long) sut.ToResult();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Print_NoParent_PrintsFinalEquality()
+    {
+        // Arrange
+        int operand = 4;
+        var sut = new Faculty(operand);
+        long factorial = 1 * 2 * 3 * 4;
+
+        string expected = $"({operand}!) = {factorial}";
+
+        // Act
+        var actual = sut.Print();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void Print_WithParent_PrintsWithoutResult()
+    {
+        int operand = 4;
+        var sut = new Faculty(operand);
+        sut.Parent = new Constant(0);
+        string expected = $"({operand}!)";
+
+        // Act
+        var actual = sut.Print();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void PrintSentence_NoParent_PrintsFinalSentence()
+    {
+        // Arrange
+        int operand = 4;
+        var sut = new Faculty(operand);
+        long factorial = 1 * 2 * 3 * 4;
+
+        string expected = $"{nameof(Faculty).ToLower()} of {operand} is {factorial}";
+
+        // Act
+        var actual = sut.PrintSentence();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void PrintSentence_WithParent_PrintsWell()
+    {
+        // Arrange
+        int operand = 4;
+        var sut = new Faculty(operand);
+        sut.Parent = new Constant(0);
+
+        string expected = $"{nameof(Faculty).ToLower()} of {operand}";
+
+        // Act
+        var actual = sut.PrintSentence();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
