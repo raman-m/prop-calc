df945c3489491ce4f26801d7e1da71b58b37cfa7
Raman Maksimchuk <dotnet044@gmail.com>
2023-02-03 22:37:12 +0300

    Pretty nice UI welcoming

--- console/Program.cs
+++ console/Program.cs
@@ -1,17 +1,47 @@
-﻿using System.Reflection;
+﻿using RamanM.Properti.Calculator.Implementations;
+using System.Reflection;
 using static System.Console;
-
-namespace Properti.Calculator.Console;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        Welcome();
+        ReadLine();
+    }
+
+    static void Welcome()
+    {
         var asm = Assembly.GetExecutingAssembly();
         var programName = asm.GetName().Name.Replace(".", " ");
 
         WriteLine();
+        ForegroundColor = ConsoleColor.Green;
+        WriteLine(asm.FullName);
+
+        var anchor = typeof(Sum);
+        WriteLine($"Loaded: {anchor.Assembly.FullName}");
+
+        WriteLine();
+        ForegroundColor = ConsoleColor.Yellow;
         WriteLine($"Welcome to {programName} app!");
-        ReadLine();
+
+        string space = anchor.Namespace;
+        var q = from t in anchor.Assembly.GetTypes()
+                where t.IsClass && t.Namespace == space
+                    && !t.Name.Contains("Operation") && !t.Name.Contains("Constant")
+                select t;
+
+        WriteLine();
+        ForegroundColor = ConsoleColor.White;
+        Write("Defined operations: ");
+        ForegroundColor = ConsoleColor.Green;
+        q.ToList().ForEach(t => Write(t.Name + ", "));
+
+        var cursor = GetCursorPosition();
+        SetCursorPosition(cursor.Left - 2, cursor.Top);
+        WriteLine(' ');
+
+        ForegroundColor = ConsoleColor.White;
     }
 }
