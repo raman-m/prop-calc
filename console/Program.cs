f9febe71d2d98da98f2c7c04ab51d1b9b8ca5004
Raman Maksimchuk <dotnet044@gmail.com>
2023-02-03 21:46:33 +0300

    Add console app as UI

---
+++ console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using static System.Console;
+
+namespace Properti.Calculator.Console;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var asm = Assembly.GetExecutingAssembly();
+        var programName = asm.GetName().Name.Replace(".", " ");
+
+        WriteLine();
+        WriteLine($"Welcome to {programName} app!");
+        ReadLine();
+    }
+}
